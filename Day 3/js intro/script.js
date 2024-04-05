90fabf3ba67c33abdb043249fab73774d3a9da5f
Anuj Singh <anujsingh92588@gmail.com>
2024-04-05 09:46:14 +0530

    4/05/24

---
+++ Day 3/js intro/script.js
@@ -0,0 +1,34 @@
+// let a=[10,20,30];
+// let c=[...a];
+// if(a===c){
+//     console.log("yes");
+// }else{
+//     console.log("no")
+// }
+
+
+// let print=(str="hello world")=>{
+//     console.log(str);
+// }
+// const s="hello";
+// print();
+
+
+// function sum(a,b){
+//     if(a&&b){
+//         console.log(a+b)
+//     }else{
+//         console.log(a)
+//     }
+// }
+// sum(1);
+
+
+// const obj={
+//     firstName: "anuj",
+//     lastName: "singh"
+// }
+// let x=prompt("Enter your name");
+// console.log(obj[x]);
+
+
